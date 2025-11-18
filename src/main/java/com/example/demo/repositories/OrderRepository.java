59607ec408cbe44cf9a05286af890f0877946b22
Raphael Salles <feijoraphael1@gmail.com>
2025-11-18 15:47:09 -0300

    Criada classe pedido com data GMT

--- src/main/java/com/example/demo/repositories/OrderRepository.java
+++ src/main/java/com/example/demo/repositories/OrderRepository.java
@@ -1,11 +1,11 @@
 package com.example.demo.repositories;
 
-import com.example.demo.entities.User;
+import com.example.demo.entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 //interface herda @Repository do JpaRepository
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
     //SpringDataJPA já tem uma implementação padrão para essa interface
 }
